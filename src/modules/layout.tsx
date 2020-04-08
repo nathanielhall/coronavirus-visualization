b5a1a74606cfe3ad446f9a031c7e21400f71e739
NATHANIEL HALL <NCHALL@ATTAIN.COM>
2020-04-08 12:18:35 -0400

    refactor data service calls

--- src/modules/layout.tsx
+++ src/modules/layout.tsx
@@ -1,23 +1,22 @@
 import React, { FC, useState, useEffect } from 'react'
-//import { Map, MapMarker } from 'components/Map'
 import { PermanentDrawer } from 'components/Drawer'
-// import { GrowthChart } from './GrowthChart'
 import { useApi } from 'src/api'
 import { List, ListItem, ListItemText } from '@material-ui/core'
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import {
-  // CountiesApi,
   StateDailyReport,
   StateReport,
   CountryDailyReport,
   CountryReport,
-  DailyReport
+  DailyReport,
+  Report,
+  NavListItem
 } from './types'
 import { getStateName } from './states'
-// import { ProvinceStatistics } from './Statistics'
 import { differenceInCalendarDays, parse } from 'date-fns'
 import { LineChart } from 'components/LineChart'
-// import { Statistics } from './Statistics'
+import { Statistics } from './Statistics'
+
 const drawerWidth = 350
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -32,114 +31,185 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 )
 
-export const Layout: FC = () => {
-  const classes = useStyles()
+export const Layout = () => {
   // const [getCounties, getCountiesResponse] = useApi<CountiesApi>(
   //   'https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=US&source=csbs'
   // )
-  const [getStates, getStatesResponse] = useApi<StateReport[]>(
+  const classes = useStyles()
+  const [requestCountry, responseCountry] = useApi<CountryReport[]>(
+    'https://covidtracking.com/api/v1/us/current.json'
+  )
+  const [requestCountryTimeline, responseCountryTimeline] = useApi<
+    CountryDailyReport[]
+  >('https://covidtracking.com/api/us/daily')
+  const [requestStates, responseStates] = useApi<StateReport[]>(
     'https://covidtracking.com/api/states'
   )
+  const [requestStatesTimeline, responseStatesTimeline] = useApi<
+    StateDailyReport[]
+  >('https://covidtracking.com/api/states/daily')
 
-  const [getUSRequest, getUSResponse] = useApi<CountryReport[]>(
-    'https://covidtracking.com/api/us'
-  )
-  const [USDailyRequest] = useApi<CountryDailyReport[]>()
-  const [StatesDailyRequest] = useApi<StateDailyReport[]>()
-  const [dailyReport, setDailyReport] = useState<DailyReport[]>()
+  const [dailyReport, setDailyReport] = useState<DailyReport[] | undefined>()
+  const [report, setReport] = useState<Report | undefined>()
+  const [navItems, setNavItems] = useState<NavListItem[] | undefined>()
+  const [selectedNavItem, setSelectedNavItem] = useState<
+    NavListItem | undefined
+  >()
 
-  const [selectedState, setSelectedState] = useState<StateReport | undefined>()
+  const onSelectionChange = (item: NavListItem) => {
+    setSelectedNavItem(item)
+  }
 
-  const listItemClick = (item: StateReport) => {
-    setSelectedState(item)
+  // Navigation List Items
+  useEffect(() => {
+    if (requestStates.loading || !responseStates) return
+    if (requestCountry.loading || !responseCountry) return
+
+    const items: NavListItem[] = responseStates.data.map((item) => ({
+      id: item.state,
+      primary: `${item.state} - ${getStateName(item.state)}`,
+      secondary: `Positive: ${item.positive.toLocaleString()} Fatalities: ${item.death.toLocaleString()}`
+    }))
+
+    console.log('set nav items', 'LOG')
+
+    const USNavItem = {
+      id: 'US',
+      primary: 'United States',
+      secondary: `Positive: ${responseCountry.data[0].positive.toLocaleString()} Fatalities: ${responseCountry.data[0].death.toLocaleString()}`
+    }
+    setNavItems([USNavItem, ...items])
+
+    setSelectedNavItem(USNavItem)
+  }, [responseStates, responseCountry])
+
+  const getStateReport = (selectedItem: string) => {
+    if (!requestStates.loading && responseStates) {
+      const item = responseStates.data.find((x) => x.state === selectedItem)
+      if (!item) return
+      console.log('set states report', 'LOG')
+      setReport({
+        title: getStateName(item.state),
+        positive: item.positive,
+        death: item.death,
+        lastModified: item.lastUpdateEt
+      })
+    }
+  }
+  const getCountryReport = () => {
+    if (!requestCountry.loading && responseCountry) {
+      console.log('set country report', 'LOG')
+
+      const { positive, death, lastModified } = responseCountry.data[0]
+      setReport({
+        title: 'United States',
+        positive,
+        death,
+        lastModified
+      })
+    }
+  }
+  const getStateTimelineReport = (selectedItem: string) => {
+    if (!requestStatesTimeline.loading && responseStatesTimeline) {
+      const data = responseStatesTimeline.data
+        .filter((x) => x.state === selectedItem)
+        .reverse()
+
+      const report: DailyReport[] = data.map((item) => ({
+        positive: item.positive,
+        death: item.death,
+        yAxis: item.positive,
+        xAxis: differenceInCalendarDays(
+          parse(item.date.toString(), 'yyyyMMdd', new Date()),
+          new Date('03/04/2020')
+        )
+      }))
+      console.log('set states daily report', 'LOG')
+
+      setDailyReport(report)
+    }
+  }
+  const getCountryTimelineReport = () => {
+    if (!requestCountryTimeline.loading && responseCountryTimeline) {
+      const d = responseCountryTimeline.data
+
+      const report: DailyReport[] = d.reverse().map((item) => ({
+        positive: item.positive,
+        death: item.death,
+        xAxis: differenceInCalendarDays(
+          parse(item.date.toString(), 'yyyyMMdd', new Date()),
+          new Date('03/04/2020') // FIXME:
+        ),
+        yAxis: item.positive
+      }))
+
+      console.log('set country daily report', 'LOG')
+
+      setDailyReport(report)
+    }
   }
 
   useEffect(() => {
-    const getStatesDaily = async (selected: string) => {
-      StatesDailyRequest.get<StateDailyReport[]>(
-        'https://covidtracking.com/api/states/daily'
-      ).then((response) => {
-        if (response.status === 200) {
-          const data = response.data
-            .filter((x) => x.state === selected)
-            .reverse()
+    //  generate report and daily report
+    if (!selectedNavItem) return
 
-          const report: DailyReport[] = data.map((item) => ({
-            positive: item.positive,
-            death: item.death,
-            yAxis: item.positive,
-            xAxis: differenceInCalendarDays(
-              parse(item.date.toString(), 'yyyyMMdd', new Date()),
-              new Date('03/04/2020')
-            )
-          }))
-          setDailyReport(report)
-        }
-      })
+    if (selectedNavItem.id === 'US') {
+      getCountryReport()
+      getCountryTimelineReport()
+    } else {
+      getStateReport(selectedNavItem.id)
+      getStateTimelineReport(selectedNavItem.id)
     }
-    const getUSDaily = async () => {
-      USDailyRequest.get<CountryDailyReport[]>(
-        'https://covidtracking.com/api/us/daily'
-      ).then((response) => {
-        if (response.status === 200) {
-          const report: DailyReport[] = response.data.reverse().map((item) => ({
-            positive: item.positive,
-            death: item.death,
-            xAxis: differenceInCalendarDays(
-              parse(item.date.toString(), 'yyyyMMdd', new Date()),
-              new Date('03/04/2020')
-            ),
-            yAxis: item.positive
-          }))
-
-          setDailyReport(report)
-        }
-      })
-    }
-
-    if (!selectedState) getUSDaily()
-    if (selectedState) getStatesDaily(selectedState.state)
-  }, [selectedState])
+  }, [selectedNavItem])
 
   return (
     <>
+      <PermanentDrawer title={'Coronavirus Visualization'} width={drawerWidth}>
+        {navItems && selectedNavItem && (
+          <LocationList
+            onChange={onSelectionChange}
+            options={navItems}
+            value={selectedNavItem}
+          />
+        )}
+      </PermanentDrawer>
       <main className={classes.content}>
+        {report && (
+          <Statistics
+            title={report.title}
+            positive={report.positive}
+            death={report.death}
+            lastModified={report.lastModified}
+          />
+        )}
         {dailyReport && (
           <LineChart data={dailyReport} xAxisKey="xAxis" yAxisKey="yAxis" />
         )}
       </main>
-      <PermanentDrawer title={'Coronavirus Visualization'} width={drawerWidth}>
-        <List>
-          {!getUSRequest.loading && getUSResponse && (
-            <ListItem
-              button
-              selected={!selectedState}
-              key={100}
-              onClick={() => setSelectedState(undefined)}
-            >
-              <ListItemText
-                primary={'United States'}
-                secondary={`Positive: ${getUSResponse.data[0].positive.toLocaleString()} Fatalities: ${getUSResponse.data[0].death.toLocaleString()}`}
-              />
-            </ListItem>
-          )}
-          {!getStates.loading &&
-            getStatesResponse &&
-            getStatesResponse.data.map((item, index) => (
-              <ListItem
-                button
-                selected={selectedState && item.state === selectedState.state}
-                key={index}
-                onClick={() => listItemClick(item)}
-              >
-                <ListItemText
-                  primary={`${item.state} - ${getStateName(item.state)}`}
-                  secondary={`Positive: ${item.positive.toLocaleString()} Fatalities: ${item.death.toLocaleString()}`}
-                />
-              </ListItem>
-            ))}
-        </List>
-      </PermanentDrawer>
     </>
   )
 }
+
+type LocationListProps = {
+  onChange: (item: NavListItem) => void
+  options: NavListItem[]
+  value: NavListItem
+}
+export const LocationList: FC<LocationListProps> = ({
+  onChange,
+  options,
+  value
+}) => (
+  <List>
+    {options.map((item) => (
+      <ListItem
+        button
+        selected={item.id === value.id}
+        key={item.id}
+        onClick={() => onChange(item)}
+      >
+        <ListItemText primary={item.primary} secondary={item.secondary} />
+      </ListItem>
+    ))}
+  </List>
+)
