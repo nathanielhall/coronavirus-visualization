464e98071122f05053abf36084c7c57ff124e4f3
NATHANIEL HALL <NCHALL@ATTAIN.COM>
2020-04-09 07:15:05 -0400

    line chart typing

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -8,17 +8,17 @@ import {
   ResponsiveContainer,
   Tooltip
 } from 'recharts'
-export type LineChartProps<TData> = {
+export type LineChartProps<TData extends object> = {
   data: TData[]
   xAxisKey: keyof TData
   yAxisKey: keyof TData
 }
 
-export const LineChart: <TData>(
+export const LineChart: <TData extends object>(
   props: LineChartProps<TData>
 ) => ReactElement<LineChartProps<TData>> = ({ data, xAxisKey, yAxisKey }) => (
   <ResponsiveContainer width={'100%'} aspect={4.0 / 1.25}>
-    <RCLineChart data={data as any[]}>
+    <RCLineChart data={data}>
       <Line type="monotone" dataKey={yAxisKey.toString()} stroke="#8884d8" />
 
       <CartesianGrid stroke="#ccc" />
